5c4c5f4f65d96f256e8a87711252868d65512c4c
Akash <shrestha.akash@gmail.com>
2016-01-05 21:58:28 +0545

    post

---
+++ scripts/app.js
@@ -0,0 +1,54 @@
+var app = angular.module('myApp', ['ngMaterial']);
+app.constant('globalVar', {cardViewsPath: 'views/cards/'});
+
+app.factory('remoteFactory', ['$http', function($http){
+    var _url="http://localhost:8888/cards/";
+    var service= {};
+
+    service.getMetaForNew = function(param){
+        var promise = $http.post(_url+'new/','data: '+param);
+        promise = promise.then(function (response) {
+            return response.data;
+        },function(response){
+          return response.data
+        });
+        return promise;
+      }
+    return service;
+
+}])
+
+app.controller('SimpleController', function($scope,remoteFactory) {
+    $scope.cards = [
+    {title:'Akash Shrestha', desc:'A Description goes here', type:'summary_large_image'},
+    {title:'Bkash Shrestha', desc:'B Description goes here', type:'gallery_card'},
+    {title:'Ckash Shrestha', desc:'C Description goes here', type:'summary_card'},
+    {title:'Dkash Shrestha', desc:'D Description goes here', type:'summary_large_image'}
+    ];
+
+    $scope.newButton = function(){
+        remoteFactory.getMetaForNew('{uri: "hamropatro://app/radio"}').then( function(data){
+            console.log(data);
+        }, function(data){
+            console.log(data);
+        });
+    };
+});
+app.directive('hpCard', ['globalVar', function(globalVar) {
+  return {
+    restrict: 'E',
+    scope: {
+      card: '=card'
+    },
+    link: function(scope, element, attrs) {
+        attrs.$observe("type",function(v){
+            scope.contentUrl = globalVar.cardViewsPath + 'card_' + v + '.html';
+        });
+    },
+    template: '<div ng-include="contentUrl"></div>'
+  };
+}]);
+app.config(function($mdIconProvider) {
+    $mdIconProvider
+      .icon('menu:more', 'images/svg/moreButton.svg',24);
+  });
